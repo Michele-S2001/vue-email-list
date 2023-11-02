432f582dcf408816fe2127b441ead6098a91f115
MicheleSerafini <serafini.michele01@gmail.com>
2023-11-02 15:03:57 +0100

    ADD > Starter files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      message: 'hello vue!'
+    }
+  },
+
+  methods: {
+    //funzioni
+    fetchEmails() {
+      //richiesta emails
+    }
+  }
+}).mount('#app')
